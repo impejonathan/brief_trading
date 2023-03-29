e4861bec6ab2bc68755ed6402e7df964fb6ad081
Evgeni1494 <yoni1@hotmail.fr>
2023-03-29 12:26:39 +0200

    main.py update

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+import sqlite3
 
 app = FastAPI()
 
+conn = sqlite3.connect("db_trading.db")
 
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: Union[bool, None] = None
 
+@app.post("/items/")
+async def create_item(item: Item):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO items (name, price) VALUES (?, ?)", (item.name, item.price))
+    conn.commit()
+    return {"item": item}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/items/")
+async def read_items():
+    cur = conn.cursor()
+    cur.execute("SELECT name, price FROM items")
+    items = cur.fetchall()
+    return {"items": items}
