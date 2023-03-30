021e9ab77e3299e3aa5c2f7b6aa12ea6591ff3ec
Evgeni1494 <yoni1@hotmail.fr>
2023-03-30 16:28:02 +0200

    mumuse avec bonus

--- main.py
+++ main.py
@@ -282,7 +282,7 @@ async def mettre_a_jour_utilisateur(utilisateur:Utilisateur,id_utilisateur:int):
     
     
 @app.put ("/mettre_a_jour_action") #OK
-async def mettre_a_jour_action(entreprise:str,prix:Action):
+async def mettre_a_jour_action(entreprise:str,prix:float):
     curseur = connexion.cursor()
     curseur.execute("""
         UPDATE action
